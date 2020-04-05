2da7109374f5be5d7bc2b5e050229eaf08113376
Piskunovich <Dmitriy106211@gmail.com>
2020-04-05 23:38:21 +0300

    8A

---
+++ src/by/it/piskunovich/_tasks_/jd01_08/Operation.java
@@ -0,0 +1,8 @@
+package by.it.piskunovich._tasks_.jd01_08;
+
+interface Operation {
+Var add(Var other);
+Var sub(Var other);
+Var mul(Var other);
+Var div(Var other);
+}
---
+++ src/by/it/piskunovich/_tasks_/jd01_08/Vector.java
@@ -0,0 +1,4 @@
+package by.it.piskunovich._tasks_.jd01_08;
+
+class Vector extends by.it.piskunovich._tasks_.jd01_07.AbstractVar {
+}
